f09b0eb8da0cee5aadaff04bb129325232414ea3
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2026-03-19 01:03:24 +0600

    data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import { useState, use } from 'react';
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 
+
+const fetchTicket = async () => {
+  const response = await fetch('/data.json');
+  const data = await response.json();
+  return data;
+}
+const ticketPromise = fetchTicket();
+
+
+
 function App() {
+  const ticketData = use(ticketPromise);
+  console.log(ticketData);
 
   return (
-    <div><button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <div>
       <Navbar />
     </div>
   )
