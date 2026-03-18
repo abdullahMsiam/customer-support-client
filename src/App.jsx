d41914ff5d6558c86ab73ac9135a3fab376403bd
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2026-03-19 00:17:27 +0600

    project initial to github

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <div><button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
